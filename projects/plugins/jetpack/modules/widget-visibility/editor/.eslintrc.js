1eb2f17abe83acbc8d16ac0ed4e817e71fbe0575
Matthew Reishus <mreishus@users.noreply.github.com>
2021-08-20 11:28:05 -0500

    Change order of eslint imports
    I was getting a lot of conflicts between eslint and prettier before
    doing it this way. (Prettier would change code to be one way, eslint
    would demand it to be another)

--- projects/plugins/jetpack/modules/widget-visibility/editor/.eslintrc.js
+++ projects/plugins/jetpack/modules/widget-visibility/editor/.eslintrc.js
@@ -1,7 +1,7 @@
 module.exports = {
 	extends: [
-		'../../../.eslintrc.js',
 		'plugin:@wordpress/eslint-plugin/recommended-with-formatting',
 		'plugin:@wordpress/eslint-plugin/i18n',
+		'../../../.eslintrc.js',
 	],
 };
